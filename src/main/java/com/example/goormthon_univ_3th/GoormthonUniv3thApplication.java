3a185832d508aecdcf814001692c579a4a6b0efb
shimfff <shimseawon1510@gmail.com>
2024-11-02 13:19:24 +0900

    init

---
+++ src/main/java/com/example/goormthon_univ_3th/GoormthonUniv3thApplication.java
@@ -0,0 +1,13 @@
+package com.example.goormthon_univ_3th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormthonUniv3thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoormthonUniv3thApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/goormthon_univ_3th/GoormthonUniv3thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goormthon_univ_3th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormthonUniv3thApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
