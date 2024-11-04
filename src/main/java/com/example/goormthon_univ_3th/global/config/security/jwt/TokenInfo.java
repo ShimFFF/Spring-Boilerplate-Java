7e222a89cff3b9c4fc723102b0f3b9e8987328af
shimfff <shimseawon1510@gmail.com>
2024-11-04 21:40:12 +0900

    :sparkles: Feat : Add TokenInfo record for handling JWT token information

---
+++ src/main/java/com/example/goormthon_univ_3th/global/config/security/jwt/TokenInfo.java
@@ -0,0 +1,17 @@
+package com.example.goormthon_univ_3th.global.config.security.jwt;
+
+import lombok.Builder;
+
+@Builder
+public record TokenInfo(
+        String grantType,
+        String accessToken,
+        String refreshToken
+) {
+
+}
+//자바 레코드란?
+//자바 14부터 릴리즈된 기능으로, 불변의 데이터를 쉽게 표현하기 위한 기능이다.
+//레코드는 클래스와 비슷하지만, 데이터를 저장하기 위한 목적으로만 사용된다.
+//레코드는 final로 선언되어 있어서 불변이다.
+//레코드는 생성자, 게터, equals, hashCode, toString 메소드를 자동으로 생성해준다.
