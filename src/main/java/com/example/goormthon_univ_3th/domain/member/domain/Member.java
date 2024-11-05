9169ba0cb385dd548aafa73211c7cd3ea0202463
shimfff <shimseawon1510@gmail.com>
2024-11-05 21:13:45 +0900

    :sparkles: Feat : Member 엔티티에 BaseEntity 상속 추가

--- src/main/java/com/example/goormthon_univ_3th/domain/member/domain/Member.java
+++ src/main/java/com/example/goormthon_univ_3th/domain/member/domain/Member.java
@@ -1,5 +1,6 @@
 package com.example.goormthon_univ_3th.domain.member.domain;
 
+import com.example.goormthon_univ_3th.global.common.base.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,7 +10,7 @@ import lombok.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/goormthon_univ_3th/domain/member/service/DetailMemberService.java
+++
@@ -1,4 +0,0 @@
-package com.example.goormthon_univ_3th.domain.member.service;
-
-public class DetailMemberService {
-}
