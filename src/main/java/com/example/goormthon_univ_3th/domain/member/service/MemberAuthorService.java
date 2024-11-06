5e3b8aab818cc1c17274fa3ea30fcfe32d14299b
shimfff <shimseawon1510@gmail.com>
2024-11-06 22:03:23 +0900

    :sparkles: Feat : Member 인증 인가 관련 서비스 인터페이스 정의

---
+++ src/main/java/com/example/goormthon_univ_3th/domain/member/service/MemberAuthorService.java
@@ -0,0 +1,21 @@
+package com.example.goormthon_univ_3th.domain.member.service;
+
+import com.example.goormthon_univ_3th.domain.member.domain.Member;
+import com.example.goormthon_univ_3th.domain.member.domain.SocialType;
+import com.example.goormthon_univ_3th.domain.member.dto.request.MemberSignUpRequest;
+import com.example.goormthon_univ_3th.domain.member.dto.response.MemberGenerateTokenResponse;
+import com.example.goormthon_univ_3th.domain.member.dto.response.MemberIdResponse;
+import com.example.goormthon_univ_3th.domain.member.dto.response.MemberLoginResponse;
+
+public interface MemberAuthorService {
+    // 소셜 로그인
+    MemberLoginResponse socialLogin(final String accessToken, SocialType socialType);
+    // 회원가입
+    MemberIdResponse signUp(Member member, MemberSignUpRequest request);
+    // 새로운 액세스 토큰 발급
+    MemberGenerateTokenResponse generateNewAccessToken(String refreshToken, Member member);
+    // 로그아웃
+    MemberIdResponse logout(Member member);
+    // 회원 탈퇴
+    MemberIdResponse withdrawal(Member member);
+}
