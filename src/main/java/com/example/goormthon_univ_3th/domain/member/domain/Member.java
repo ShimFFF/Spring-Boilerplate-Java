a2f0f876687951f9105fa61548a946813b641237
shimfff <shimseawon1510@gmail.com>
2024-11-06 22:32:43 +0900

    :sparkles: Feat : Member 엔티티에 로그인 정보 관련 필드 추가

--- src/main/java/com/example/goormthon_univ_3th/domain/member/domain/Member.java
+++ src/main/java/com/example/goormthon_univ_3th/domain/member/domain/Member.java
@@ -12,17 +12,27 @@ public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String email;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    private String clientId;
+
+
     @Builder
-    public Member(String name, String email) {
+    public Member(String name, String email, SocialType socialType, String clientId) {
         this.name = name;
         this.email = email;
         this.role = Role.MEMBER;
+        this.socialType = socialType;
+        this.clientId = clientId;
     }
 
 
