714f8b6dbb0be79e0d4309eaea854697b73510d4
shimfff <shimseawon1510@gmail.com>
2024-11-06 22:40:35 +0900

    :sparkles: Feat : 인증 인가 처리 관련 api DTO에 필드 생성

--- src/main/java/com/example/goormthon_univ_3th/domain/member/dto/request/MemberSignUpRequest.java
+++ src/main/java/com/example/goormthon_univ_3th/domain/member/dto/request/MemberSignUpRequest.java
@@ -1,4 +1,8 @@
 package com.example.goormthon_univ_3th.domain.member.dto.request;
 
+import lombok.Getter;
+
+@Getter
 public class MemberSignUpRequest {
+    private String name;
 }
--- src/main/java/com/example/goormthon_univ_3th/domain/member/dto/response/MemberLoginResponse.java
+++ src/main/java/com/example/goormthon_univ_3th/domain/member/dto/response/MemberLoginResponse.java
@@ -1,4 +1,14 @@
 package com.example.goormthon_univ_3th.domain.member.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+
+@Getter
+@Builder
 public class MemberLoginResponse {
+    private Long memberId;
+    private String accessToken;
+    private String refreshToken;
+    private boolean isServiceMember;
 }
